fca0e2a386480ef84f67970eb34b65447aabf03a
Yunis-Mammadov <yunismammadovv@gmail.com>
2025-07-22 12:09:57 +0400

    implement footer components and finish home page structure

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,13 @@
 const Footer = () => {
   return (
-    <div>
-        THIS IS Footer
+    <div className="footer-container">
+      <div className="footer-title">
+        <h1>Click here to explore similar projects.</h1>
+        <button className="login-btn">Visit Now</button>
+      </div>
+      <div>
+        <p className="footer-text">© Copyright 2025 Upstudy | Developed By Devthrow.</p>
+      </div>
     </div>
   )
 }
