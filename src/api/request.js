c9bbecd8f33eaf2a7713d18b6f7d74eb644c779e
Yunis-Mammadov <yunismammadovv@gmail.com>
2025-08-19 11:31:12 +0400

    baseUrl updated v1

--- src/api/request.js
+++ src/api/request.js
@@ -2,23 +2,23 @@ import axios from 'axios';
 import { BASE_URL } from './baseUrl'
 
 export const signUp = async (payload) => {
-    const response = await axios.post(`${BASE_URL}/auth/user`, payload);
+    const response = await axios.post(`${BASE_URL}/api/auth/user`, payload);
     return response;
 };
 
 export const signIn = async (payload) => {
-    const response = await axios.post(`${BASE_URL}/auth/login`, payload)
+    const response = await axios.post(`${BASE_URL}/api/auth/login`, payload)
     return response
 };
 
 
 export const getAllFaqs = async () => {
-    const response = await axios.get(`${BASE_URL}/faqs`)
+    const response = await axios.get(`${BASE_URL}/api/faqs`)
     return response.data
 };
 
 
 export const getAllCards = async() => {
-    const response = await axios.get(`${BASE_URL}/cards`)
+    const response = await axios.get(`${BASE_URL}/api/cards`)
     return response.data
 };
